16ba22ea5f427dd65127d2c8806b184747f5eb27
Your Name <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>
2024-02-16 16:54:51 +0530

    soem changes

--- models/cartModel.js
+++ models/cartModel.js
@@ -1,22 +1,29 @@
 const mongoose = require('mongoose');
+
 const cartSchema = mongoose.Schema({
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
     unique: true
   },
-  products:{
-    product:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'SellerProduct',
+  products: [{
+    _id: false, // Disable _id field for subdocuments
+    product: {
+      type: String
     },
-    quantity:{type:Number}
-  },
-isDeleted: {
+    quantity: {
+      type: Number
+    }
+  }],
+  isDeleted: {
     type: Boolean,
     default: false
   }
 }, {
   timestamps: true,
-})
-module.exports = mongoose.model('Cart', cartSchema)
+});
+
+module.exports = mongoose.model('Cart', cartSchema);
+
+ // type: mongoose.Schema.Types.ObjectId,
+        // ref: 'SellerProduct',
--- server.js
+++ server.js
@@ -13,7 +13,9 @@ app.use(cors(corsOptions));
 const errorMiddleware = require('./middleware/error-middleware');
 
 //connect database
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const userModel = require('./models/userModel');
+const cartModel = require('./models/cartModel');
 mongoose.connect(process.env.MONGOURL).then(() => {
   console.log("Successfully connect to MongoDB.");
 })
@@ -44,6 +46,140 @@ app.use('/api/localseller', require('./routes/localseller/localseller.routes'))
 app.get('/', async(req, res) => {
   res.send('localbazaars Backend Run Successfully!')
 })
+
+
+
+// app.post('/addtocart', async(req, res) => {
+// try {
+//   // const data = [{
+//   //   product: "65cf3d9a4d369ec3fc7437db",
+//   //   quantity: 4
+//   // },
+//   // {
+//   //   product: "65cf3d9a4d369ec3fc7437db3",
+//   //   quantity: 2
+//   // },
+//   // {
+//   //   product: "65cf3d9a4d369ec3fc7437db2",
+//   //   quantity: 7
+//   // },
+//   // {
+//   //   product: "65cf3d9a4d369ec3fc7437db1",
+//   //   quantity: 5
+//   // },
+//   // ];
+
+//   const additionalData = [{
+//     product: "65cf3d9a4d369ec3fc7437dc",
+//     quantity: 3
+//   },
+//   {
+//     product: "65cf3d9a4d369ec3fc7437dc2",
+//     quantity: 1
+//   }];
+
+  
+// const user=await userModel.findOne().select('_id')
+// // Find the existing cart document by user ID
+// console.log(user)
+// await cartModel.findOne({ user: user?._id })
+//   .then(cart => {
+//     if (cart) {
+//       console.log(cart)
+//       // Update the products array
+//       // cart.products = data.map(item => ({
+//       //   product: item.product,
+//       //   quantity: item.quantity
+//       // }));
+//       // Append the additional products to the existing products array
+//             additionalData.forEach(item => {
+//               cart.products.push({
+//                 product: item.product,
+//                 quantity: item.quantity
+//               });
+//             });
+      
+
+//       // Save the updated cart
+//       return cart.save();
+//     } else {
+//       // If the cart doesn't exist, create a new one
+//       const newCart = new cartModel({
+//         user: user?._id,
+//         products: data.map(item => ({
+//           product: item.product,
+//           quantity: item.quantity
+//         }))
+//       });
+
+//       return newCart.save();
+//     }
+//   })
+//   .then(updatedCart => {
+//     console.log('Cart updated successfully:', updatedCart);
+//     res.status(200).send(updatedCart)
+//   })
+//   .catch(error => {
+//     console.error('Error updating cart:', error);
+//   }); 
+// } catch (error) {
+//   console.log(error.message)
+//   res.status(500).send(error.message)
+// }
+// })
+
+
+app.post('/addtocart', async (req, res) => {
+  try {
+    const additionalData = [{
+      product: "65cf3d9a4d369ecdsfsd3fc7437dcwgwegewegw",
+      quantity: 3
+    },
+    {
+      product: "65cf3d9a4d369ecvdsvdsvdvs3fcgewgewge7437dc2",
+      quantity: 1
+    }];
+
+    // Assuming you have a user ID
+    const user = await userModel.findOne().select('_id');
+    console.log(user);
+
+    // Find the existing cart document by user ID
+    let cart = await cartModel.findOne({ user: user?._id });
+
+    if (cart) {
+      // Append the additional products to the existing products array
+      additionalData.forEach(item => {
+        cart.products.push({
+          product: item.product,
+          quantity: item.quantity
+        });
+      });
+      // Save the updated products array
+      cart = await cart.save();
+      console.log('Cart updated successfully:', cart);
+      res.status(200).send(cart);
+    } else {
+      // If the cart doesn't exist, create a new one
+      const newCart = new cartModel({
+        user: user?._id,
+        products: additionalData.map(item => ({
+          product: item.product,
+          quantity: item.quantity
+        }))
+      });
+      const savedCart = await newCart.save();
+      console.log('New cart created:', savedCart);
+      res.status(200).send(savedCart);
+    }
+  } catch (error) {
+    console.error('Error updating cart:', error);
+    res.status(500).send(error.message);
+  }
+});
+
+
+
 app.use(errorMiddleware);
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, function () {
